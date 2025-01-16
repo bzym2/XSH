22661001ca12c51a63945fa6b9ebaa68f747fc0c
Stevesuk0 <stevesukawa@outlook.com>
2025-01-17 02:47:03 +0800

    feature: export command for test

--- shell.py
+++ shell.py
@@ -3,6 +3,7 @@ import getpass
 import platform
 
 theme = "bash"
+registery = {}
 
 print("Welcome to Hush. A sleek, ultra-lightweight, and extensible shell.")
 
@@ -16,7 +17,6 @@ def main():
     while True:
         shinput = input(getPrefix(theme))
 
-
         if not shinput:
             pass
         
@@ -28,7 +28,17 @@ def main():
                 os.chdir(shinput.split(' ')[-1])
             except:
                 print(f'hush: cd: can\'t cd to {shinput.split(" ")[-1]}')
-        
+    
+        elif shinput.startswith('export '):
+            parts = shinput.split(' ')
+
+            variable_name = parts[1].split('=')[0]
+            variable_value = parts[1].split('=')[1].strip("'")
+
+            print(f"Name : {variable_name}")
+            print(f"Value: {variable_value}")
+
+
         else:
             os.system(shinput)
 
