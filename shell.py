5b49e91ebf3066d36c676db0ea2951cd09c9b5f1
Stevesuk0 <stevesukawa@outlook.com>
2025-01-17 20:23:25 +0800

    feature: sex with me so amazing

--- shell.py
+++ shell.py
@@ -5,18 +5,24 @@ import platform
 import readline # keyboard support
 import subprocess
 import globalvars
+import colorama
+
+colorama.init()
 
 print(globalvars.logo_colossal)
 print(globalvars.logo_text)
 
 registery = {}
 
+style = {}
+
 def getPrefix(theme: str = ''):
+    global style
     style = {
         'bash': f"{getpass.getuser()}@{platform.node()}:{os.getcwd().replace(os.path.expanduser('~'), '~')}# ",
-        'kali': f"┌──({getpass.getuser()}㉿kali)-[{os.getcwd().replace(os.path.expanduser('~'), '~')}]\n└──$ ",
-        'hush': f"┌──[{getpass.getuser()} on daSH]\n└──{os.getcwd().replace(os.path.expanduser('~'), '~')}> ",
-        'omega': f"┌──(ø@{getpass.getuser()})\n└──{os.getcwd().replace(os.path.expanduser('~'), '~')}> "
+        'kali': f"{colorama.Fore.BLUE}┌──({getpass.getuser()}㉿{colorama.Fore.RED}kali{colorama.Fore.BLUE})-[{os.getcwd().replace(os.path.expanduser('~'), '~')}]\n└──{colorama.Fore.RESET}$ ",
+        'hush': f"╭─[{getpass.getuser()} on Hush]\n╰─{os.getcwd().replace(os.path.expanduser('~'), '~')}> ",
+        'omega': f"╭──(ø@{getpass.getuser()})\n╰──{os.getcwd().replace(os.path.expanduser('~'), '~')}> "
     }
     return style.get(theme, '$ ')
 
@@ -37,13 +43,25 @@ def processVariable(command: str):
     return command
 
 def completer(text, state):
-    commands = os.listdir()
+    commands = os.listdir()  
     if text:
-        matches = [cmd for cmd in commands if cmd.startswith(text)]
+        matches = []
+        for cmd in commands:
+            if cmd.lower().startswith(text.lower()):
+                matches.append(cmd)
+        
+        custom_commands = ['_listvar', '_theme_list', '_theme_set', 'cd', 'exit', 'quit', 'export', '_checkfile']
+        for custom_cmd in custom_commands:
+            if custom_cmd.lower().startswith(text.lower()):
+                matches.append(custom_cmd)
     else:
         matches = commands
+
     try:
-        return matches[state]
+        if matches:
+            return matches[state]
+        else:
+            return None
     except IndexError:
         return None
 
@@ -75,18 +93,55 @@ def main():
             for i in registery:
                 print(f"{i}: {registery[i]}")
 
-        elif shinput.startswith('_themechanger'):
+        elif shinput.startswith('_theme_set'):
             theme = shinput.split(' ')[-1]
+
+        elif shinput.startswith('_theme_list'):
+            print("List of theme available:\n")
+            for i in style:
+                print(f"Theme \"{i}\" preview:", end=f'\n{style[i]}\n')
+
+            print()
     
         elif shinput.startswith('export '):
             parts = shinput.split(' ')
 
             registery[parts[1].split('=')[0]] = parts[1].split('=')[1].strip()
-
+        elif shinput.startswith('_checkfile '):
+            parts = shinput.split(' ')
+            flag = False
+            for i in registery['PATH'].split(':'):
+                if os.path.exists(f'{i}/{parts[1]}'):
+                    flag = True
+                    break
+            if os.path.exists(f"{os.getcwd()}/{parts[1]}"):
+                flag = True
+            if flag:
+                print("exist")
+            else:
+                print("non")
+            
         else:
-            subprocess.run(shinput, env=registery, shell=True)
+            parts = shinput.split(' ')
+            flag = False
+            for i in registery['PATH'].split(':'):
+                if os.path.exists(f'{i}/{parts[0]}'):
+                    flag = True
+                    break
+            if os.path.exists(f"{os.getcwd()}/{parts[0]}"):
+                flag = True
+            if flag:
+                subprocess.run(parts, env=registery, shell=False)
+            else:
+                print("hush: File do not exists, please check your command and try again.")
+                
+            # subprocess.run(shinput, env=registery, shell=True)
+                
 
 
-
-main()
-
+while True:
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nGoodbye")
+        exit(0)
